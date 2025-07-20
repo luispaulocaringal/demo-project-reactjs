58cfd6ae700ff233b21b3b5eb8fc240cbec24a88
lpcaringal <luis.caringal@senarysoft.com>
2025-07-21 02:34:29 +0800

    added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css'
 
 import Auth from './pages/Auth/Auth'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
 
+const router = createBrowserRouter([
+  { path: '/', element: <Home/> },
+  { path: '/login', element: <Auth/> }
+])
+
 function App() {
   return (
     <>
-      {/* <Auth /> */}
-      <Home/>
+      <RouterProvider router={router}/>
       <Footer />
     </>
   )
